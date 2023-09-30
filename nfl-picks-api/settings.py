39db18a4d266376f56f23cb08afe5e5ff4b01ace
alextowe <alextowe96@gmail.com>
2023-09-30 16:10:30 -0400

    renamed project folder

--- nfl-picks-api/settings.py
+++ nfl-picks-api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pickem.urls'
+ROOT_URLCONF = 'nfl-picks-api.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pickem.wsgi.application'
+WSGI_APPLICATION = 'nfl-api-picks.wsgi.application'
 
 
 # Database
